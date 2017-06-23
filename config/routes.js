2e49d413e372f7d91486f00da1233287796dffab
KRBHylian <kevinrolinbenitez@gmail.com>
2017-06-23 23:15:06 +0200

    Erreur 404

--- config/routes.js
+++ config/routes.js
@@ -50,7 +50,15 @@ module.exports.routes = {
 
   '/regles': {
     view: 'regles'
-  }
+  },
+
+  'get /login': { view: 'user/login' },
+  'get /signup': { view: 'user/signup' },
+  '/welcome': { view: 'user/welcome' },
+  'post /login': 'UserController.login',
+  'post /signup': 'UserController.signup',
+  '/logout': 'UserController.logout'
+}
 
   /***************************************************************************
   *                                                                          *
